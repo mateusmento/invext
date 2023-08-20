0c0d6217c929003a8852113b9deb9c08d96cd1c1
Mateus Sarmento <omateusmento@gmail.com>
2023-08-20 18:13:17 -0300

    Allow methods in cors config

--- invext-api/src/main/java/com/invext/application/configurations/CorsConfig.java
+++ invext-api/src/main/java/com/invext/application/configurations/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000");
+                    .allowedOrigins("http://localhost:3000")
+					.allowedMethods("*");
 			}
 		};
 	}
