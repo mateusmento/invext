45bbe21cfa3e36358ecdf6c1b6120e6bc4a2f70d
Mateus Sarmento <omateusmento@gmail.com>
2023-08-20 18:19:28 -0300

    Toggle between client and attendant link

--- invext-app/src/Root.js
+++ invext-app/src/Root.js
@@ -1,13 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import './App.css';
 
 export function Root() {
+  const location = useLocation();
+
   return (
     <div className="App">
       <header>
         <h1>INVEXT</h1>
-        <a className="button" href="/attendant">Sou atendente</a>
-        <a className="button" href="/client">Sou cliente</a>
+        { location.pathname === '/client'
+          ? <a className="button" href="/attendant">Sou atendente</a>
+          : <a className="button" href="/client">Sou cliente</a>
+        }
       </header>
       <main>
         <Outlet/>
