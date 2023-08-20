0a75d71882e8d7ec962e006cc87372110a455028
Mateus Sarmento <omateusmento@gmail.com>
2023-08-20 09:45:55 -0300

    Add  constructors to Attendant

--- invext-api/src/main/java/com/invext/domain/entities/Attendant.java
+++ invext-api/src/main/java/com/invext/domain/entities/Attendant.java
@@ -8,11 +8,15 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter @Setter
 @Entity
 public class Attendant {
