c1dbbd045ce79034a8b7fc71014d44ebc18ebca6
Mateus Sarmento <omateusmento@gmail.com>
2023-08-20 14:15:32 -0300

    Add service and awaiting section

--- invext-app/src/App.js
+++ invext-app/src/App.js
@@ -1,14 +1,24 @@
 import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+function createServiceRequest(serviceRequestData) {
+  return axios.post('http://localhost:8080/service-requests', serviceRequestData);
+}
 
 function App() {
   const [clientName, setClientName] = useState('');
-  const [serviceType, setServiceType] = useState();
   const [step, setStep] = useState('clientName');
+  const [serviceRequest, setServiceRequest] = useState();
 
-  function selectServiceType(serviceType) {
-    setServiceType(serviceType);
-    setStep('service');
+  async function requestService(serviceType) {
+    const { data: serviceRequest } = await createServiceRequest({ serviceType, clientName });
+    if (serviceRequest.attendant) {
+      setStep('service');
+    } else {
+      setStep('await-service');
+    }
+    setServiceRequest(serviceRequest);
   }
 
   return (
@@ -32,12 +42,23 @@ function App() {
         { step === 'serviceType' && (
           <>
             <h2>Que tipo de assunto você deseja falar?</h2>
-            <div class="service-types">
-              <button onClick={() => selectServiceType('CARD_PROBLEMS')}>Problema de Cartões</button>
-              <button onClick={() => selectServiceType('LOAN_CONTRACTING')}>Contratação de emprestimo</button>
-              <button onClick={() => selectServiceType('OTHERS')}>Outros Assuntos</button>
+            <div className="service-types">
+              <button onClick={() => requestService('CARD_PROBLEMS')}>Problema de Cartões</button>
+              <button onClick={() => requestService('LOAN_CONTRACTING')}>Contratação de emprestimo</button>
+              <button onClick={() => requestService('OTHERS')}>Outros Assuntos</button>
             </div>
             <button onClick={() => setStep('clientName')}>Voltar</button>
+          </>
+        )}
+        { step === 'service' && (
+          <>
+            <h2>Você está falando com {serviceRequest.attendant.name}</h2>
+          </>
+        )}
+        { step === 'await-service' && (
+          <>
+            <h2>No momento, nossos atendentes estão todos ocupados.</h2>
+            <div>Aguarde até um atendente ficar disponível, por favor.</div>
           </>
         )}
       </main>
