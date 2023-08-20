25c41c1e1e090229a863582d09cc797460921f8b
Mateus Sarmento <omateusmento@gmail.com>
2023-08-20 20:02:19 -0300

    Move files to components

--- invext-app/src/index.js
+++ invext-app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Client from './Client';
+import Client from './components/Client';
 import { RouterProvider, createBrowserRouter, redirect } from 'react-router-dom';
-import { Attendant } from './Attendant';
-import { Root } from './Root';
+import { Attendant } from './components/Attendant';
+import { Root } from './components/Root';
 
 const router = createBrowserRouter([
     {
